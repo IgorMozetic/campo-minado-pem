1c9952fbb38449ebe67844dd3561ecbea1d6823f
jhonattanferreira <jhonattan.ferreira@hiremavericks.ai>
2025-08-14 19:17:05 -0300

    fix: adjust font-size

--- src/components/board/CellStyle.js
+++ src/components/board/CellStyle.js
@@ -43,11 +43,15 @@ export const Button = styled.button`
     }
   }};
   display: block;
-  font-size: ${({ cellCode }) => (cellCode > 0 ? "2vw" : "3vw")};
+  font-size: ${({ cellCode }) => (cellCode > 0 ? "0.9vw" : "1.5vw")};
   font-weight: bold;
   outline: none;
   width: 100%;
   height: 100%;
   min-width: 40px;
   min-height: 40px;
+  max-font-size: ${({ cellCode }) => (cellCode > 0 ? "12px" : "16px")};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
