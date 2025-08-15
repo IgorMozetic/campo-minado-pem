f7eeb619e02a1a7565536044ed1d6efc96252c4f
jhonattanferreira <jhonattan.ferreira@hiremavericks.ai>
2025-08-15 01:22:27 -0300

    chore: small touches

--- src/components/board/BoardStyle.js
+++ src/components/board/BoardStyle.js
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   grid-template-columns: repeat(${props => props.widthSize || 9}, 1fr);
   gap: ${CELL_MARGIN}px;
   justify-content: center;
-  margin: 10px auto 0 auto;
+  margin: 30px auto 0 auto;
   width: 90vw;
   height: 70vh;
   max-width: 90vh;
